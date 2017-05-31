dcfd5ac400c6fd8b0d96d0a7b938881aeed96635
BoyGK <1215356195@qq.com>
2017-05-31 15:21:50 +0800

    添加jdbcutil

---
+++ src/com/gkpoter/dazuoye/test/Main.java
@@ -0,0 +1,28 @@
+package com.gkpoter.dazuoye.test;
+
+import com.gkpoter.dazuoye.util.JdbcUtil;
+
+import java.sql.*;
+
+/**
+ * Created by 12153 on 2017/5/31.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            JdbcUtil jdbcUtil=JdbcUtil.getInstance();
+            Connection conn = jdbcUtil.getConnection();
+            PreparedStatement ps = conn.prepareStatement("select * from test");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                System.out.println("id : " + rs.getInt(1) + " name : "
+                        + rs.getString("username") + " password : " + rs.getString(3));
+            }
+            jdbcUtil.closeConnection(rs,ps,conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
